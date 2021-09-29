017142160e2887e173d4858cd14e2bfc2a9eb812
Gabriel-Belloni <gabriel-belloni@hotmail.com>
2021-09-29 15:12:09 -0300

    Update calculator-tests.js

--- Project-CALCULADORA-TDD-JS/tdd-mocha-chai-main/tests/calculator-tests.js
+++ Project-CALCULADORA-TDD-JS/tdd-mocha-chai-main/tests/calculator-tests.js
@@ -8,28 +8,29 @@ describe('The Calculator', () => {
         calculator = new Calculator
     });
 
-    it('Should sum two positive integers', () => {
+    it('Deve somar dois numeros inteiros e retornar uma variavel number', () => {
       expect(calculator.sum(2, 3)).to.equal(5);
       expect(calculator.div(6, 3)).to.be.a("number");
     });
 
-    it('Should sub two positive integers', () => {
+    it('Deve subtrair dois numeros inteiros e retornar uma variavel number', () => {
       expect(calculator.sub(5, 3)).to.equal(2);
       expect(calculator.div(6, 3)).to.be.a("number");
     });
 
-    it('Should mult two positive integers', () => {
+    it('Deve multiplicar dois numeros inteiros e retornar uma variavel number', () => {
       expect(calculator.mult(5, 3)).to.equal(15);
       expect(calculator.div(6, 3)).to.be.a("number");
     });
 
-    it('Deve fazer a divisao de dois numeros inteiros', () => {
+    it('Deve dividir dois numeros inteiros e retornar uma variavel number', () => {
       expect(calculator.div(6, 3)).to.equal(2);
       expect(calculator.div(6, 3)).to.be.a("number");
     });
 
-    it('Deve ser feito a raiz quadrada de um numero inteiro', () => {
+    it('Deve ser feito a raiz quadrada de um numero inteiro e retornar uma variavel number', () => {
       expect(calculator.sqrt(49)).to.equal(7);
+      expect(calculator.div(6, 3)).to.be.a("number");
 
     });
 });
